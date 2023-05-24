7b3e3501cd46c381c81c9ce0fcec14620c484db5
PavelBagaev <70954796+PavelBagaev@users.noreply.github.com>
2023-05-24 19:43:45 +0300

    Add files via upload

---
+++ AbstractClassInstance/AbstractClassInstance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace AbstractClassInstance
+{
+    abstract class AbsractClass
+    {
+        public AbsractClass()
+        {
+            Console.WriteLine("Instance of the abstract class: " + this.GetType().Name + " has been created. Yay!");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = (AbsractClass)typeof(RuntimeTypeHandle).GetMethod("Allocate", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, new object[] { typeof(AbsractClass) });
+            typeof(AbsractClass).GetConstructor(Type.EmptyTypes).Invoke(obj, new object[0]);
+            Console.ReadLine();
+        }
+    }
+}
