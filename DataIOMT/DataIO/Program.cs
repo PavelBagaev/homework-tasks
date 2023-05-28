09b0f07421cf4c21626eeb6428fec9ae17b2e5fd
PavelBagaev <70954796+PavelBagaev@users.noreply.github.com>
2023-05-28 22:15:22 +0300

    Add files via upload

---
+++ DataIOMT/DataIO/Program.cs
@@ -0,0 +1,54 @@
+﻿using DataProcessing;
+using System.Net.Http.Json;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace DataIO
+{
+    internal class Program
+    {
+        static void Main(string[] args) //путь к текстовому файлу следует передать в аргументе командной строки
+        {
+            try
+            {
+                string line;
+                Dictionary<string, string> sortedMap = new Dictionary<string, string>();
+
+                List<string> book = new List<string>();
+
+                StreamReader streamReader = new StreamReader(args[0]);
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    book.Add(line);
+                }
+                streamReader.Close();
+
+                HttpClient client = new HttpClient();
+                var uri = "https://localhost:7048/TextProcessor";
+                string jsonBook = JsonSerializer.Serialize(book);
+                var content = new StringContent(jsonBook, Encoding.UTF8, "application/json");
+                var response = client.PostAsync(uri, content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content.ReadAsStringAsync().Result.Split(",");
+                    sortedMap = responseContent.Select(item => item.Split(':')).ToDictionary(s => s[0], s => s[1]);                  
+                }
+
+                StreamWriter streamWriter = new StreamWriter("result.txt");
+
+                foreach (var entry in sortedMap)
+                {
+                    streamWriter.WriteLine(String.Format("{0,-30}{1}", entry.Key, entry.Value));
+                }
+                streamWriter.Close();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ DataProcessingMT/DataProcessing/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace DataProcessing
+{
+    public class DataProcessor
+    {        
+        public Dictionary<string, int> ProcessData(List<string> book)
+        {           
+            string tmp;
+            Regex regex = new Regex("[^\\p{L}'-]+");
+            List<string> wordsList = new List<string>();
+            ConcurrentBag<string> wordsListConcurrent = new ConcurrentBag<string>();
+            Dictionary<string, int> occurrencesMap = new Dictionary<string, int>();
+            ConcurrentDictionary<string, int> occurancesMapConcurrent = new ConcurrentDictionary<string, int>();
+            int numberOfOccurrences = 1;
+            
+            Parallel.For(0, book.Count, i =>
+            {
+                tmp = book[i];
+                List<string> words = regex.Split(tmp).ToList<string>();
+                words.RemoveAll(word => word.Equals("") || word.Equals("--") || words.Equals("-"));
+                foreach(string word in words.ConvertAll(word => word.ToLower()))
+                {
+                    wordsListConcurrent.Add(word);
+                }                            
+            });
+            wordsList = wordsListConcurrent.ToList();
+            
+            Parallel.For(0, wordsList.Count, i =>
+            {
+                if (!occurancesMapConcurrent.ContainsKey(wordsList[i]))
+                {
+                    occurancesMapConcurrent.TryAdd(wordsList[i], numberOfOccurrences);
+                }
+                else
+                {
+                    occurancesMapConcurrent[wordsList[i]]++;
+                }
+            });           
+
+            var sortedMap = occurancesMapConcurrent.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            return sortedMap;
+        }
+    }
+}
